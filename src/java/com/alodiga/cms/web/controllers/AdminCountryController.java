3c4d5d695aa15217d6d429ff8046fef9790603a3
Unknown <lgutierrez@alodiga.com>
2019-11-13 17:50:45 -0400

    actualizacion del controlador de paises

--- src/java/com/alodiga/cms/web/controllers/AdminCountryController.java
+++ src/java/com/alodiga/cms/web/controllers/AdminCountryController.java
@@ -11,7 +11,10 @@ import com.cms.commons.models.Country;
 import com.cms.commons.models.Currency;
 import com.cms.commons.util.EJBServiceLocator;
 import com.cms.commons.util.EjbConstants;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Sessions;
@@ -25,24 +28,23 @@ public class AdminCountryController extends GenericAbstractAdminController {
     private static final long serialVersionUID = -9145887024839938515L;
     private Textbox txtName;
     private Textbox txtCode;
-    private Textbox txtCodeIso2;
-    private Textbox txtCodeIso3;
+    private Textbox txtShortName;
+    private Textbox txtAlternativeName1;
     private UtilsEJB utilsEJB = null;
     private Combobox cmbCurrency;
     private Country countryParam;
     private Button btnSave;
     private Integer eventType;
-    
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
         countryParam = (Sessions.getCurrent().getAttribute("object") != null) ? (Country) Sessions.getCurrent().getAttribute("object") : null;
-        eventType = (Integer) Sessions.getCurrent().getAttribute( WebConstants.EVENTYPE);
+        eventType = (Integer) Sessions.getCurrent().getAttribute(WebConstants.EVENTYPE);
         initialize();
         //initView(eventType, "sp.crud.country");
     }
-    
+
     @Override
     public void initialize() {
         super.initialize();
@@ -56,19 +58,17 @@ public class AdminCountryController extends GenericAbstractAdminController {
 
     public void clearFields() {
         txtName.setRawValue(null);
-        txtCodeIso2.setRawValue(null);
+        txtShortName.setRawValue(null);
         txtCode.setRawValue(null);
-        txtCodeIso3.setRawValue(null);
-
-//Cambio prueba
+        txtAlternativeName1.setRawValue(null);
     }
 
     private void loadFields(Country country) {
         try {
             txtName.setText(country.getName());
-            txtCodeIso2.setText(country.getCodeIso2());
+            txtShortName.setText(country.getCodeIso2());
             txtCode.setText(country.getCode());
-            txtCodeIso3.setText(country.getCodeIso3());
+            txtAlternativeName1.setText(country.getCodeIso3());
 
         } catch (Exception ex) {
             showError(ex);
@@ -77,9 +77,9 @@ public class AdminCountryController extends GenericAbstractAdminController {
 
     public void blockFields() {
         txtName.setReadonly(true);
-        txtCodeIso2.setReadonly(true);
+        txtShortName.setReadonly(true);
         txtCode.setReadonly(true);
-        txtCodeIso3.setReadonly(true);
+        txtAlternativeName1.setReadonly(true);
         btnSave.setVisible(false);
     }
 
@@ -87,8 +87,8 @@ public class AdminCountryController extends GenericAbstractAdminController {
         if (txtName.getText().isEmpty()) {
             txtName.setFocus(true);
             this.showMessage("sp.error.field.cannotNull", true, null);
-        } else if (txtCodeIso2.getText().isEmpty()) {
-            txtCodeIso2.setFocus(true);
+        } else if (txtShortName.getText().isEmpty()) {
+            txtShortName.setFocus(true);
             this.showMessage("sp.error.field.cannotNull", true, null);
         } else if (txtCode.getText().isEmpty()) {
             txtCode.setFocus(true);
@@ -111,7 +111,6 @@ public class AdminCountryController extends GenericAbstractAdminController {
     private void saveCountry(Country _country) {
         try {
             Country country = null;
-            
 
             if (_country != null) {
                 country = _country;
@@ -120,16 +119,15 @@ public class AdminCountryController extends GenericAbstractAdminController {
             }
             country.setName(txtName.getText());
             country.setCode(txtCode.getText());
-            country.setCodeIso2(txtCodeIso2.getText());
-            country.setCodeIso3(txtCodeIso3.getText());
-            country.setCurrencyId((Currency) cmbCurrency.getSelectedItem().getValue());
+            country.setCodeIso2(txtShortName.getText());
+            country.setCodeIso3(txtAlternativeName1.getText());
+            country.setCurrencyId((Currency) cmbCurrency.getSelectedItem().getValue());//prueba
             country = utilsEJB.saveCountry(country);
             countryParam = country;
             this.showMessage("sp.common.save.success", false, null);
         } catch (Exception ex) {
-           showError(ex);
+            showError(ex);
         }
-
     }
 
     public void onClick$btnSave() {
@@ -151,32 +149,34 @@ public class AdminCountryController extends GenericAbstractAdminController {
         switch (eventType) {
             case WebConstants.EVENT_EDIT:
                 loadFields(countryParam);
+                loadCmbCurrency(eventType);
                 break;
             case WebConstants.EVENT_VIEW:
                 loadFields(countryParam);
                 txtName.setDisabled(true);
-                txtCodeIso2.setDisabled(true);
+                txtShortName.setDisabled(true);
                 txtCode.setDisabled(true);
-                txtCodeIso3.setDisabled(true);
+                txtAlternativeName1.setDisabled(true);
                 loadCmbCurrency(eventType);
                 break;
             case WebConstants.EVENT_ADD:
+                loadCmbCurrency(eventType);
                 break;
             default:
                 break;
         }
     }
-    
+
     private void loadCmbCurrency(Integer evenInteger) {
         //cmbCurrency
         EJBRequest request1 = new EJBRequest();
         List<Currency> currencies;
- 
+
         try {
             currencies = utilsEJB.getCurrency(request1);
             cmbCurrency.getItems().clear();
             for (Currency c : currencies) {
- 
+
                 Comboitem item = new Comboitem();
                 item.setValue(c);
                 item.setLabel(c.getSymbol());
@@ -190,9 +190,8 @@ public class AdminCountryController extends GenericAbstractAdminController {
                 cmbCurrency.setSelectedIndex(1);
             } if (evenInteger.equals(WebConstants.EVENT_VIEW)) {
                 cmbCurrency.setDisabled(true);
-            }
-            
-            
+            } 
+            //prueba luly
         } catch (EmptyListException ex) {
             showError(ex);
             ex.printStackTrace();
@@ -203,7 +202,5 @@ public class AdminCountryController extends GenericAbstractAdminController {
             showError(ex);
             ex.printStackTrace();
         }
- 
     }
-
 }
