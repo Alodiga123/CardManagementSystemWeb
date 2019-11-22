b5cf0a8aae233b9de74da6a6367bd84db714b9d6
Darwin <darwinm_1988@hotmail.com>
2019-11-22 15:23:16 -0430

    Cambios en etiquetas de vista Programs

--- src/java/com/alodiga/cms/web/controllers/AdminProgramController.java
+++ src/java/com/alodiga/cms/web/controllers/AdminProgramController.java
@@ -34,44 +34,41 @@ import org.zkoss.zul.Button;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Comboitem;
 import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Radiogroup;
 import org.zkoss.zul.Textbox;
 
 public class AdminProgramController extends GenericAbstractAdminController {
 
     private static final long serialVersionUID = -9145887024839938515L;
-    private Textbox    txtName;
-    private Textbox    txtdescription;
-    private Datebox    dtbcontdate;
-    private Combobox   cmbProgramType;
-    private Combobox   cmbProductType;
-    private Combobox   cmbIssuer;
-    private Combobox   cmbProgramOwner;
-    private Combobox   cmbBinSponsor;
-    private Combobox   cmbCardType;
-    private Textbox    website;
-  //  private radiogroup branded;
-   // private radiogroup reloadable;
-    private Combobox   cmbSourceOfFound;
-    private Textbox    txtother;
-   // private radiogroup CashAcces;
-  //  private radiogroup international;
-    private Combobox   cmbNetWork;
-    private Textbox    txtOtherNetWork;
-    private Textbox    txtbin;
-    private Combobox   cmbCurrency;
-    private Combobox   cmbresponsibleNetwoork;
-    private Textbox    txtotheBINN;
-    private Combobox   cmbcardIssuanceType;
-    private Datebox    dtbexpectedLaunchDate;
+    private Textbox txtName;
+    private Textbox txtDescription;
+    private Datebox dtbContdate;
+    private Combobox cmbProgramType;
+    private Combobox cmbProductType;
+    private Combobox cmbIssuer;
+    private Combobox cmbProgramOwner;
+    private Combobox cmbBinSponsor;
+    private Combobox cmbCardType;
+    private Textbox website;
+    private Radiogroup branded;
+    private Radiogroup reloadable;
+    private Combobox cmbSourceOfFound;
+    private Textbox txtOther;
+    private Radiogroup CashAcces;
+    private Radiogroup international;
+    private Combobox cmbNetWork;
+    private Textbox txtOtherNetWork;
+    private Textbox txtBin;
+    private Combobox cmbCurrency;
+    private Combobox cmbResponsibleNetwoork;
+    private Textbox txtOtheBINN;
+    private Combobox cmbCardIssuanceType;
+    private Datebox dtbExpectedLaunchDate;
     private ProgramEJB programEJB = null;
-    private final UtilsEJB utilsEJB = null;
+    private UtilsEJB utilsEJB = null;
     private Program programParam;
     private Button btnSave;
     private Integer eventType;
-
-
-    
-    
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
@@ -87,7 +84,7 @@ public class AdminProgramController extends GenericAbstractAdminController {
         super.initialize();
         try {
             programEJB = (ProgramEJB) EJBServiceLocator.getInstance().get(EjbConstants.PROGRAM_EJB);
-           loadData();
+            loadData();
         } catch (Exception ex) {
             showError(ex);
         }
@@ -95,28 +92,28 @@ public class AdminProgramController extends GenericAbstractAdminController {
 
     public void clearFields() {
         txtName.setRawValue(null);
-        txtdescription.setRawValue(null);
-        dtbcontdate.setRawValue(null);
+        txtDescription.setRawValue(null);
+        dtbContdate.setRawValue(null);
         website.setRawValue(null);
         //Faltan los radio buton
-        txtother.setRawValue(null);
+        txtOther.setRawValue(null);
         txtOtherNetWork.setRawValue(null);
-        txtbin.setRawValue(null);
-        txtotheBINN.setRawValue(null);
-        dtbexpectedLaunchDate.setRawValue(null);
+        txtBin.setRawValue(null);
+        txtOtheBINN.setRawValue(null);
+        dtbExpectedLaunchDate.setRawValue(null);
 //Cambio prueba
     }
 
     private void loadFields(Program program) {
         try {
             txtName.setText(program.getName());
-            txtdescription.setText(program.getDescription());
-            dtbcontdate.setValue(program.getContractDate());
+            txtDescription.setText(program.getDescription());
+            dtbContdate.setValue(program.getContractDate());
             website.setText(program.getWebSite());
-            txtother.setText(program.getOtherSourceFunds());
-           txtOtherNetWork.setText(program.getOtherResponsibleNetworkReporting());
-           txtbin.setText(program.getBiniinNumber());
-            dtbexpectedLaunchDate.setValue(program.getExpectedLaunchDate());
+            txtOther.setText(program.getOtherSourceFunds());
+            txtOtherNetWork.setText(program.getOtherResponsibleNetworkReporting());
+            txtBin.setText(program.getBiniinNumber());
+            dtbExpectedLaunchDate.setValue(program.getExpectedLaunchDate());
 
         } catch (Exception ex) {
             showError(ex);
@@ -125,14 +122,14 @@ public class AdminProgramController extends GenericAbstractAdminController {
 
     public void blockFields() {
         txtName.setReadonly(true);
-        txtdescription.setReadonly(true);
-        dtbcontdate.setReadonly(true);
+        txtDescription.setReadonly(true);
+        dtbContdate.setReadonly(true);
         website.setReadonly(true);
-        txtother.setReadonly(true);
+        txtOther.setReadonly(true);
         txtOtherNetWork.setReadonly(true);
-        txtbin.setReadonly(true);
-        dtbexpectedLaunchDate.setReadonly(true);
-        
+        txtBin.setReadonly(true);
+        dtbExpectedLaunchDate.setReadonly(true);
+
         btnSave.setVisible(false);
     }
 
@@ -140,8 +137,8 @@ public class AdminProgramController extends GenericAbstractAdminController {
         if (txtName.getText().isEmpty()) {
             txtName.setFocus(true);
             this.showMessage("sp.error.field.cannotNull", true, null);
-        } else if (txtdescription.getText().isEmpty()) {
-            txtdescription.setFocus(true);
+        } else if (txtDescription.getText().isEmpty()) {
+            txtDescription.setFocus(true);
             this.showMessage("sp.error.field.cannotNull", true, null);
 
         } else {
@@ -159,39 +156,35 @@ public class AdminProgramController extends GenericAbstractAdminController {
         Executions.getCurrent().sendRedirect("/docs/countries-abbreviation.pdf", "_blank");
     }
 
-    private void saveProgram( Program _program  )  {
+    private void saveProgram(Program _program) {
         try {
             Program program = null;
 
-            if (_program!= null) {
+            if (_program != null) {
                 program = _program;
             } else {//New country
                 program = new Program();
             }
-            /*
-            private Textbox txtName;
-    private Textbox txtdescription;
-    private Textbox txtcontdate;
-             */
+
             program.setName(txtName.getText());
-            program.setDescription(txtdescription.getText());
-            program.setContractDate(dtbcontdate.getValue());
+            program.setDescription(txtDescription.getText());
+            program.setContractDate(dtbContdate.getValue());
             program.setProgramTypeId((ProgramType) cmbProgramType.getSelectedItem().getValue());
             program.setProductTypeId((ProductType) cmbProductType.getSelectedItem().getValue());
             program.setIssuerId((Issuer) cmbIssuer.getSelectedItem().getValue());
             program.setBinSponsorId((BinSponsor) cmbBinSponsor.getSelectedItem().getValue());
-            //cmbCardType
-           // program.((CardType) cmbCardType.getSelectedItem().getValue());
-           // program.
-            
-            
-           program = programEJB.saveProgram(program);
+            //facta laCardType
+            program.setWebSite(website.getText());
+
+            // program.((CardType) cmbCardType.getSelectedItem().getValue());
+            // program.
+            program = programEJB.saveProgram(program);
             programParam = program;
             this.showMessage("sp.common.save.success", false, null);
         } catch (WrongValueException ex) {
             showError(ex);
-        
-    }   catch (GeneralException ex) {
+
+        } catch (GeneralException ex) {
             Logger.getLogger(AdminProgramController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (RegisterNotFoundException ex) {
             Logger.getLogger(AdminProgramController.class.getName()).log(Level.SEVERE, null, ex);
@@ -199,11 +192,8 @@ public class AdminProgramController extends GenericAbstractAdminController {
             Logger.getLogger(AdminProgramController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        
-         } 
-        
-        
-       
+    }
+
     public void onClick$btnSave() {
         if (validateEmpty()) {
             switch (eventType) {
@@ -211,7 +201,7 @@ public class AdminProgramController extends GenericAbstractAdminController {
                     saveProgram(null);
                     break;
                 case WebConstants.EVENT_EDIT:
-                     saveProgram(programParam);
+                    saveProgram(programParam);
                     break;
                 default:
                     break;
@@ -219,126 +209,94 @@ public class AdminProgramController extends GenericAbstractAdminController {
         }
     }
 
-   
-
-    
- 
     public void loadData() {
         switch (eventType) {
             case WebConstants.EVENT_EDIT:
                 loadFields(programParam);
-                loadcmbProgram(eventType);
                 loadCmbCurrency(eventType);
-                loadcmbProductType(eventType);
-                loadcmbProductType(eventType);
-                loadcmbIssuer(eventType);
-                loadcmbProgramOwner(eventType);
-                loadcmbBinSponsor(eventType);
-                loadcmbCardType(eventType);
-                loadcmbSourceOfFound(eventType);
-                loadcmbNetWork(eventType);
-                loadcmbCurrency(eventType);
-                loadcmbresponsibleNetwoork(eventType);
-                loadcmbcardIssuanceType(eventType);
+                loadCmbProductType(eventType);
+                loadCmbProgramType(eventType);
+                loadCmbIssuer(eventType);
+                loadCmbProgramOwner(eventType);
+                loadCmbBinSponsor(eventType);
+                loadCmbCardType(eventType);
+                loadCmbSourceOfFound(eventType);
+                loadCmbNetWork(eventType);
+                loadCmbresponsibleNetwoork(eventType);
+                loadCmbcardIssuanceType(eventType);
 
                 break;
             case WebConstants.EVENT_VIEW:
                 loadFields(programParam);
                 txtName.setDisabled(true);
-                txtdescription.setDisabled(true);
-                dtbcontdate.setDisabled(true);
-                loadcmbProgram(eventType);
+                txtDescription.setDisabled(true);
+                dtbContdate.setDisabled(true);
+                website.setDisabled(true);
+                txtOther.setDisabled(true);
+                txtOtherNetWork.setDisabled(true);
+                txtBin.setDisabled(true);
+                txtOtheBINN.setDisabled(true);
+                //me faltan los radio grp
                 loadCmbCurrency(eventType);
-                loadcmbProgramType(eventType);
-                loadcmbProductType(eventType);
-                loadcmbIssuer(eventType);
-                loadcmbProgramOwner(eventType);
-                loadcmbBinSponsor(eventType);
-                loadcmbCardType(eventType);
-                loadcmbSourceOfFound(eventType);
-                loadcmbNetWork(eventType);
-                loadcmbCurrency(eventType);
-                loadcmbresponsibleNetwoork(eventType);
-                loadcmbcardIssuanceType(eventType);
+                loadCmbProgramType(eventType);
+                loadCmbProductType(eventType);
+                loadCmbIssuer(eventType);
+                loadCmbProgramOwner(eventType);
+                loadCmbBinSponsor(eventType);
+                loadCmbCardType(eventType);
+                loadCmbSourceOfFound(eventType);
+                loadCmbNetWork(eventType);
+                loadCmbresponsibleNetwoork(eventType);
+                loadCmbcardIssuanceType(eventType);
                 break;
             case WebConstants.EVENT_ADD:
-                loadcmbProgram(eventType);
+
                 loadCmbCurrency(eventType);
-                loadcmbProgramType(eventType);
-                loadcmbProductType(eventType);
-                loadcmbIssuer(eventType);
-                loadcmbProgramOwner(eventType);
-                loadcmbBinSponsor(eventType);
-                loadcmbCardType(eventType);
-                loadcmbSourceOfFound(eventType);
-                loadcmbNetWork(eventType);
-                loadcmbCurrency(eventType);
-                loadcmbresponsibleNetwoork(eventType);
-                loadcmbcardIssuanceType(eventType);
+                loadCmbProgramType(eventType);
+                loadCmbProductType(eventType);
+                loadCmbIssuer(eventType);
+                loadCmbProgramOwner(eventType);
+                loadCmbBinSponsor(eventType);
+                loadCmbCardType(eventType);
+                loadCmbSourceOfFound(eventType);
+                loadCmbNetWork(eventType);
+                loadCmbresponsibleNetwoork(eventType);
+                loadCmbcardIssuanceType(eventType);
 
             default:
                 break;
         }
     }
 
-    private void loadcmbProgram(Integer evenInteger) {
-   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    
+    private void loadCmbIssuer(Integer evenInteger) {
+
     }
 
-    private void loadcmbProductType(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void loadCmbBinSponsor(Integer evenInteger) {
+
     }
 
-    private void loadcmbIssuer(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void loadCmbSourceOfFound(Integer evenInteger) {
+
     }
 
-    private void loadcmbProgramOwner(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void loadCmbresponsibleNetwoork(Integer evenInteger) {
+
     }
 
-    private void loadcmbBinSponsor(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    private void loadCmbcardIssuanceType(Integer evenInteger) {
 
-    private void loadcmbCardType(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbSourceOfFound(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbNetWork(Integer eventType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbCurrency(Integer eventType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbresponsibleNetwoork(Integer eventType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbcardIssuanceType(Integer eventType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void loadcmbProgramType(Integer evenInteger) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     private void loadCmbCurrency(Integer evenInteger) {
-         EJBRequest request1 = new EJBRequest();
+        EJBRequest request1 = new EJBRequest();
         List<Currency> currencies;
- 
+
         try {
             currencies = utilsEJB.getCurrency(request1);
             cmbCurrency.getItems().clear();
             for (Currency c : currencies) {
- 
+
                 Comboitem item = new Comboitem();
                 item.setValue(c);
                 item.setLabel(c.getSymbol());
@@ -350,11 +308,11 @@ public class AdminProgramController extends GenericAbstractAdminController {
             }
             if (evenInteger.equals(WebConstants.EVENT_ADD)) {
                 cmbCurrency.setSelectedIndex(1);
-            } if (evenInteger.equals(WebConstants.EVENT_VIEW)) {
+            }
+            if (evenInteger.equals(WebConstants.EVENT_VIEW)) {
                 cmbCurrency.setDisabled(true);
             }
-            
-            
+
         } catch (EmptyListException ex) {
             showError(ex);
         } catch (GeneralException ex) {
@@ -362,18 +320,58 @@ public class AdminProgramController extends GenericAbstractAdminController {
         } catch (NullParameterException ex) {
             showError(ex);
         }
- 
+
     }
 
-        
-        
-        
+    private void loadCmbProgramType(Integer evenInteger) {
+        EJBRequest request1 = new EJBRequest();
+        List<ProgramType> programTypes;
+
+        try {
+            programTypes = utilsEJB.getProgramType(request);
+            cmbProgramType.getItems().clear();
+            for (ProgramType c : programTypes) {
+
+                Comboitem item = new Comboitem();
+                item.setValue(c);
+
+                item.setDescription(c.getName());
+                item.setParent(cmbCurrency);
+                if (programParam != null && c.getId().equals(programParam.getProductTypeId().getId())) {
+                    cmbProgramType.setSelectedItem(item);
+                }
+            }
+            if (evenInteger.equals(WebConstants.EVENT_ADD)) {
+                cmbProgramType.setSelectedIndex(1);
+            }
+            if (evenInteger.equals(WebConstants.EVENT_VIEW)) {
+                cmbProgramType.setDisabled(true);
+            }
+
+        } catch (EmptyListException ex) {
+            showError(ex);
+        } catch (GeneralException ex) {
+            showError(ex);
+        } catch (NullParameterException ex) {
+            showError(ex);
+        }
+
     }
 
-    //public void loadData() {
-       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    
+    private void loadCmbNetWork(Integer evenInteger) {
 
-   
+    }
 
+    private void loadCmbProductType(Integer evenInteger) {
 
+    }
+
+    private void loadCmbProgramOwner(Integer evenInteger) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void loadCmbCardType(Integer evenInteger) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
