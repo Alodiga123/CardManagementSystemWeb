89f924a9fefaeccafede36f1f3073f4e2d6408df
Jesús Gómez <jedugomvil@hotmail.com>
2020-01-16 11:29:06 -0400

    Listado de Tárifas Generales

---
+++ src/java/com/alodiga/cms/web/controllers/ListGeneralRateController.java
@@ -0,0 +1,157 @@
+package com.alodiga.cms.web.controllers;
+
+import com.alodiga.cms.commons.ejb.ProductEJB;
+import com.alodiga.cms.commons.ejb.RequestEJB;
+import com.alodiga.cms.commons.ejb.UtilsEJB;
+import com.alodiga.cms.commons.exception.EmptyListException;
+import com.alodiga.cms.commons.exception.GeneralException;
+import com.alodiga.cms.commons.exception.NullParameterException;
+import com.alodiga.cms.web.custom.components.ListcellEditButton;
+import com.alodiga.cms.web.custom.components.ListcellViewButton;
+import com.alodiga.cms.web.generic.controllers.GenericAbstractListController;
+import static com.alodiga.cms.web.generic.controllers.GenericDistributionController.request;
+import com.alodiga.cms.web.utils.Utils;
+import com.alodiga.cms.web.utils.WebConstants;
+import com.cms.commons.models.GeneralRate;
+import com.cms.commons.models.Request;
+import com.cms.commons.models.RequestType;
+import com.cms.commons.models.User;
+import com.cms.commons.util.Constants;
+import com.cms.commons.util.EJBServiceLocator;
+import com.cms.commons.util.EjbConstants;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Textbox;
+
+public class ListGeneralRateController extends GenericAbstractListController<Request> {
+
+    private static final long serialVersionUID = -9145887024839938515L;
+    private Listbox lbxRecords;
+    private Textbox txtRequestNumber;
+    private ProductEJB productEJB = null;
+    private List<GeneralRate> generalRateList = null;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        initialize(); 
+    }
+
+    public void startListener() {
+    }
+
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        try {
+            //Evaluar Permisos
+            permissionEdit = true;
+            permissionAdd = true; 
+            permissionRead = true;
+            adminPage = "AdminGeneralRate.zul";
+            productEJB = (ProductEJB) EJBServiceLocator.getInstance().get(EjbConstants.PRODUCT_EJB);
+            getData();
+            loadList(generalRateList);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+ 
+    public void onClick$btnAddGeneralRate() throws InterruptedException {
+        Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_ADD);
+        Executions.getCurrent().sendRedirect(adminPage);
+    }
+
+    public void onClick$btnDelete() {
+    }
+
+    public void loadList(List<GeneralRate> list) {
+        String applicantName = "";
+        try {
+            lbxRecords.getItems().clear();
+            Listitem item = null;
+            if (list != null && !list.isEmpty()) {
+                for (GeneralRate generalRate : list) {
+                    item = new Listitem();
+                    item.setValue(generalRate);
+                    item.appendChild(new Listcell(generalRate.getCountryId().getName()));
+                    item.appendChild(new Listcell(generalRate.getProductTypeId().getName()));
+                    item.appendChild(new Listcell(generalRate.getChannelId().getName()));
+                    item.appendChild(new Listcell(generalRate.getTransactionId().getDescription()));
+                    item.appendChild(new Listcell(generalRate.getFixedRate().toString()));
+                    item.appendChild(permissionEdit ? new ListcellEditButton(adminPage, generalRate) : new Listcell());
+                    item.appendChild(permissionRead ? new ListcellViewButton(adminPage, generalRate) : new Listcell());
+                    item.setParent(lbxRecords);
+                }
+            } else {
+                btnDownload.setVisible(false);
+                item = new Listitem();
+                item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.setParent(lbxRecords);
+            }
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void getData() {
+        generalRateList = new ArrayList<GeneralRate>();
+        try {
+            request.setFirst(0);
+            request.setLimit(null);
+            generalRateList = productEJB.getGeneralRate(request);
+        } catch (NullParameterException ex) {
+            showError(ex);
+        } catch (EmptyListException ex) {
+           showEmptyList();
+        } catch (GeneralException ex) {
+            showError(ex);
+        }
+    }
+    
+    
+    private void showEmptyList(){
+                Listitem item = new Listitem();
+                item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.setParent(lbxRecords);  
+    }
+
+    public void onClick$btnDownload() throws InterruptedException {
+        try {
+            Utils.exportExcel(lbxRecords, Labels.getLabel("cms.common.cardRequest.list"));
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void onClick$btnClear() throws InterruptedException {
+        txtRequestNumber.setText("");
+    }
+
+    @Override
+    public List<Request> getFilterList(String filter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void loadDataList(List<Request> list) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
