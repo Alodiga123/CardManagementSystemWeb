8dc5c48785892fe2598b2e263eb6db7d07f989f5
Unknown <lgutierrez@alodiga.com>
2020-05-23 03:33:24 -0400

    Inventario

---
+++ src/java/com/alodiga/cms/web/controllers/ListCardInventoryControllers.java
@@ -0,0 +1,243 @@
+package com.alodiga.cms.web.controllers;
+
+import com.alodiga.cms.commons.ejb.CardEJB;
+import com.alodiga.cms.commons.ejb.UtilsEJB;
+import com.alodiga.cms.commons.exception.EmptyListException;
+import com.alodiga.cms.commons.exception.GeneralException;
+import com.alodiga.cms.commons.exception.NullParameterException;
+import com.alodiga.cms.commons.exception.RegisterNotFoundException;
+import com.alodiga.cms.web.custom.components.ListcellEditButton;
+import com.alodiga.cms.web.custom.components.ListcellViewButton;
+import com.alodiga.cms.web.generic.controllers.GenericAbstractListController;
+import com.alodiga.cms.web.utils.Utils;
+import com.alodiga.cms.web.utils.WebConstants;
+import com.cms.commons.genericEJB.EJBRequest;
+import com.cms.commons.models.Card;
+import com.cms.commons.models.CardDeliveryRegister;
+import com.cms.commons.models.CardStatus;
+import com.cms.commons.models.DeliveryRequest;
+import com.cms.commons.models.DeliveryRequetsHasCard;
+import com.cms.commons.models.User;
+import com.cms.commons.util.Constants;
+import com.cms.commons.util.EJBServiceLocator;
+import com.cms.commons.util.EjbConstants;
+import com.cms.commons.util.QueryConstants;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.EventQueue;
+import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+public class ListCardInventoryControllers extends GenericAbstractListController<Card> {
+
+    private static final long serialVersionUID = -9145887024839938515L;
+    private Listbox lbxRecords;
+    private Textbox txtName;
+    private UtilsEJB utilsEJB = null;
+    private CardEJB cardEJB = null;
+    private List<Card> card = null;
+    private User currentUser;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        initialize();
+        startListener();
+    }
+    
+     public void startListener() {
+        EventQueue que = EventQueues.lookup("updateCardInventory", EventQueues.APPLICATION, true);
+        que.subscribe(new EventListener() {
+
+            public void onEvent(Event evt) {
+                getData();
+                loadDataList(card);
+            }
+        });
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        try {
+            permissionEdit = true;
+            permissionAdd = true;
+            permissionRead = true;
+            currentUser = (User) session.getAttribute(Constants.USER_OBJ_SESSION);
+            adminPage = "adminCardInventory.zul";
+            utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
+            cardEJB = (CardEJB) EJBServiceLocator.getInstance().get(EjbConstants.CARD_EJB);
+            getData();
+            loadDataList(card);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void getData() {
+        card = new ArrayList<Card>();
+        try {
+
+            EJBRequest request2 = new EJBRequest();
+            Map params = new HashMap();
+            params = new HashMap();
+//            pendiente para validar la constante porque se trae el estatus 2            
+//            params.put(QueryConstants.PARAM_CARDS_STATUS_ID, Constants.STATUS_CARDS_PERSONALIZED);
+            params.put(QueryConstants.PARAM_CARDS_STATUS_ID, 2);
+            request2.setParams(params);
+
+            card = cardEJB.getCardByStatus(request2);
+
+        } catch (NullParameterException ex) {
+            showError(ex);
+        } catch (EmptyListException ex) {
+            showEmptyList();
+        } catch (GeneralException ex) {
+            showError(ex);
+        }
+    }
+
+    public void onClick$btnAdd() throws InterruptedException {
+        try {
+            Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_ADD);
+            Map<String, Object> paramsPass = new HashMap<String, Object>();
+            paramsPass.put("object", card);
+            final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+            window.doModal();
+        } catch (Exception ex) {
+            this.showMessage("sp.error.general", true, ex);
+        }
+    }
+
+    public void onClick$btnDownload() throws InterruptedException {
+        try {
+            Utils.exportExcel(lbxRecords, Labels.getLabel("sp.crud.enterprise.list"));
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void onClick$btnClear() throws InterruptedException {
+        txtName.setText("");
+    }
+
+    public void loadDataList(List<Card> list) {
+        Listcell tmpCell = new Listcell();
+        try {
+            lbxRecords.getItems().clear();
+            Listitem item = null;
+            if (list != null && !list.isEmpty()) {
+                btnDownload.setVisible(true);
+                for (Card card : list) {
+
+                    item = new Listitem();
+                    item.setValue(card);
+                    String pattern = "yyyy-MM-dd";
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+                    item.appendChild(new Listcell(card.getCardNumber()));
+                    item.appendChild(new Listcell(card.getCardHolder()));
+                    item.appendChild(new Listcell(simpleDateFormat.format(card.getExpirationDate())));
+                    item.appendChild(new Listcell(card.getCardStatusId().getDescription()));
+//                    item.appendChild(new ListcellEditButton(adminPage, card));
+//                    item.appendChild(new ListcellViewButton(adminPage, card, true));
+                    item.appendChild(createButtonEditModal(card));
+                    item.appendChild(createButtonViewModal(card));
+                    item.setParent(lbxRecords);
+                }
+            } else {
+                btnDownload.setVisible(false);
+                item = new Listitem();
+                item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.setParent(lbxRecords);
+            }
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+    
+        public Listcell createButtonEditModal(final Object obg) {
+        Listcell listcellEditModal = new Listcell();
+        try {
+            Button button = new Button();
+            button.setImage("/images/icon-edit.png");
+            button.setTooltiptext(Labels.getLabel("sp.common.actions.edit"));
+            button.setClass("open orange");
+            button.addEventListener("onClick", new EventListener() {
+                @Override
+                public void onEvent(Event arg0) throws Exception {
+                    Sessions.getCurrent().setAttribute("object", obg);
+                    Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_EDIT);
+                    Map<String, Object> paramsPass = new HashMap<String, Object>();
+                    paramsPass.put("object", obg);
+                    final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+                    window.doModal();
+                }
+            });
+            button.setParent(listcellEditModal);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listcellEditModal;
+    }
+
+    public Listcell createButtonViewModal(final Object obg) {
+        Listcell listcellViewModal = new Listcell();
+        try {
+            Button button = new Button();
+            button.setImage("/images/icon-invoice.png");
+            button.setTooltiptext(Labels.getLabel("sp.common.actions.view"));
+            button.setClass("open orange");
+            button.addEventListener("onClick", new EventListener() {
+                @Override
+                public void onEvent(Event arg0) throws Exception {
+                    Sessions.getCurrent().setAttribute("object", obg);
+                    Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_VIEW);
+                    Map<String, Object> paramsPass = new HashMap<String, Object>();
+                    paramsPass.put("object", obg);
+                    final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+                    window.doModal();
+                }
+            });
+            button.setParent(listcellViewModal);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listcellViewModal;
+    }
+
+    private void showEmptyList() {
+        Listitem item = new Listitem();
+        item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+        item.appendChild(new Listcell());
+        item.appendChild(new Listcell());
+        item.appendChild(new Listcell());
+        item.setParent(lbxRecords);
+    }
+
+    @Override
+    public List<Card> getFilterList(String filter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
