eeca90e0c93cec81f71bdaa34207e86460582cb2
Unknown <lgutierrez@alodiga.com>
2020-06-05 19:36:34 -0400

    controladores nuevos y ajustes

--- src/java/com/alodiga/cms/web/controllers/AdminCardRenewalControllers.java
+++ src/java/com/alodiga/cms/web/controllers/AdminCardRenewalControllers.java
@@ -1,56 +1,42 @@
 package com.alodiga.cms.web.controllers;
 
 import com.alodiga.cms.commons.ejb.CardEJB;
-import com.alodiga.cms.commons.ejb.UtilsEJB;
-import com.alodiga.cms.commons.exception.EmptyListException;
-import com.alodiga.cms.commons.exception.GeneralException;
-import com.alodiga.cms.commons.exception.NullParameterException;
 import com.alodiga.cms.web.generic.controllers.GenericAbstractAdminController;
 import com.alodiga.cms.web.utils.WebConstants;
-import com.cms.commons.genericEJB.EJBRequest;
 import com.cms.commons.models.Card;
-import com.cms.commons.models.Country;
-import com.cms.commons.models.Issuer;
+import com.cms.commons.models.CardRenewalRequest;
+import com.cms.commons.models.CardRenewalRequestHasCard;
 import com.cms.commons.util.EJBServiceLocator;
 import com.cms.commons.util.EjbConstants;
 import java.text.SimpleDateFormat;
-import java.util.List;
-import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zul.Button;
-import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Label;
-import org.zkoss.zul.Toolbarbutton;
+import org.zkoss.zul.Textbox;
 
 public class AdminCardRenewalControllers extends GenericAbstractAdminController {
 
     private static final long serialVersionUID = -9145887024839938515L;
-    private Label lblIdentificationNumber;
-    private Label lblCarHolder;
-    private Label lblCardNumber;
-    private Label lblProgram;
-    private Label lblProduct;
+    private Label lblNumberRequest;
+    private Label lblDate;
     private Label lblStatus;
-    private Label lblExpirationDate;
-    private Label lblIssueDate;
-    private Combobox cmbCountry;
-    private Combobox cmbIssuer;
-    private UtilsEJB utilsEJB = null;
+    private Textbox txtObservations;
     private CardEJB cardEJB = null;
     private Card cardParam;
+    private CardRenewalRequestHasCard cardRenewalParam;
+    public static CardRenewalRequestHasCard cardRenewalRequestHasCardParent = null;
     private Button btnSave;
     private Integer eventType;
-    private Toolbarbutton tbbTitle;
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
         eventType = (Integer) (Sessions.getCurrent().getAttribute(WebConstants.EVENTYPE));
         if (eventType == WebConstants.EVENT_ADD) {
-            cardParam = null;
+            cardRenewalParam = null;
         } else {
-            cardParam = (Card) Sessions.getCurrent().getAttribute("object");
+            cardRenewalParam = (CardRenewalRequestHasCard) Sessions.getCurrent().getAttribute("object");
         }
         initialize();
         loadData();
@@ -59,44 +45,33 @@ public class AdminCardRenewalControllers extends GenericAbstractAdminController 
     @Override
     public void initialize() {
         super.initialize();
-        switch (eventType) {
-            case WebConstants.EVENT_EDIT:
-                tbbTitle.setLabel(Labels.getLabel("cms.crud.cardRenewal.edit"));
-                break;
-            case WebConstants.EVENT_VIEW:
-                tbbTitle.setLabel(Labels.getLabel("cms.crud.cardRenewal.view"));
-                break;
-            case WebConstants.EVENT_ADD:
-                tbbTitle.setLabel(Labels.getLabel("cms.crud.cardRenewal.add"));
-                break;
-            default:
-                break;
-        }
         try {
-            utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
             cardEJB = (CardEJB) EJBServiceLocator.getInstance().get(EjbConstants.CARD_EJB);
         } catch (Exception ex) {
             showError(ex);
         }
     }
-
-    public void clearFields() {
+    
+    public CardRenewalRequestHasCard getCardRenewalRequestHasCard() {
+        return cardRenewalRequestHasCardParent;
     }
 
-    private void loadFields(Card card) {
+    public void clearFields() {
+        txtObservations.setRawValue(null);
+    }
+
+    private void loadFields(CardRenewalRequestHasCard cardRenawal) {
         try {
             String pattern = "yyyy-MM-dd";
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
-//            lblIdentificationNumber.setValue(card.getPersonCustomerId());
-            lblCarHolder.setValue(card.getCardHolder());
-            lblProgram.setValue(card.getProgramId().getName());
-            lblProduct.setValue(card.getProductId().getName());
-            lblCardNumber.setValue(card.getCardNumber());
-            lblExpirationDate.setValue(simpleDateFormat.format(card.getExpirationDate()));
-            lblIssueDate.setValue(simpleDateFormat.format(card.getIssueDate()));
-            lblStatus.setValue(card.getCardStatusId().getDescription());
-
+            lblNumberRequest.setValue(cardRenawal.getCardRenewalRequestId().getRequestNumber());
+            lblDate.setValue(simpleDateFormat.format(cardRenawal.getCardRenewalRequestId().getRequestDate()));
+            lblStatus.setValue(cardRenawal.getCardRenewalRequestId().getStatusCardRenewalRequestId().getDescription());
+            if (cardRenawal.getCardRenewalRequestId() != null) {
+                txtObservations.setText(cardRenawal.getCardRenewalRequestId().getObservations());
+            }
+            cardRenewalRequestHasCardParent = cardRenawal;
             btnSave.setVisible(true);
         } catch (Exception ex) {
             showError(ex);
@@ -104,33 +79,36 @@ public class AdminCardRenewalControllers extends GenericAbstractAdminController 
     }
 
     public void blockFields() {
+        txtObservations.setReadonly(true);
         btnSave.setVisible(false);
     }
 
     public Boolean validateEmpty() {
-        if (cmbCountry.getSelectedItem() == null) {
-            cmbCountry.setFocus(true);
-            this.showMessage("cms.error.country.notSelected", true, null);
-        } else if (cmbIssuer.getSelectedItem() == null) {
-            cmbIssuer.setFocus(true);
-            this.showMessage("cms.error.Issuer.notSelected", true, null);
+        if (txtObservations.getText().isEmpty()) {
+            txtObservations.setFocus(true);
+            this.showMessage("sp.error.field.cannotNull", true, null);
         } else {
             return true;
         }
         return false;
     }
 
-    private void saveCardStatus(Card _card) {
+    private void saveCardRenewal(CardRenewalRequest _cardRenawal) {
         try {
-            Card card = null;
+            CardRenewalRequestHasCard cardRenawalHasCard = null;
+            CardRenewalRequest cardRenawal = null;
 
-            if (_card != null) {
-                card = _card;
-            } else {//New requestType
-                card = new Card();
+            if (_cardRenawal != null) {
+                cardRenawal = _cardRenawal;
+            } else {//New country
+                cardRenawal = new CardRenewalRequest();
             }
 
+            cardRenawal.setObservations(txtObservations.getText());
+            cardRenawal = cardEJB.saveCardRenewalRequest(cardRenawal);
+
             this.showMessage("sp.common.save.success", false, null);
+            
             btnSave.setVisible(false);
         } catch (Exception ex) {
             showError(ex);
@@ -141,10 +119,13 @@ public class AdminCardRenewalControllers extends GenericAbstractAdminController 
         if (validateEmpty()) {
             switch (eventType) {
                 case WebConstants.EVENT_ADD:
-                    saveCardStatus(null);
+                    saveCardRenewal(null);
                     break;
                 case WebConstants.EVENT_EDIT:
-                    saveCardStatus(cardParam);
+                    saveCardRenewal(cardRenewalParam.getCardRenewalRequestId());
+                    cardRenewalRequestHasCardParent = cardRenewalParam;
+                    break;
+                default:
                     break;
             }
         }
@@ -153,56 +134,17 @@ public class AdminCardRenewalControllers extends GenericAbstractAdminController 
     public void loadData() {
         switch (eventType) {
             case WebConstants.EVENT_EDIT:
-                loadFields(cardParam);
-                loadCmbCountry(eventType);
-                loadCmbIssuer(eventType);
+                loadFields(cardRenewalParam);
                 break;
             case WebConstants.EVENT_VIEW:
-                loadFields(cardParam);
+                loadFields(cardRenewalParam);
                 blockFields();
-                loadCmbCountry(eventType);
-                loadCmbIssuer(eventType);
                 break;
             case WebConstants.EVENT_ADD:
-                loadCmbCountry(eventType);
-                loadCmbIssuer(eventType);
                 break;
             default:
                 break;
         }
     }
 
-    private void loadCmbCountry(Integer evenInteger) {
-        EJBRequest request1 = new EJBRequest();
-        List<Country> countries;
-        try {
-            countries = utilsEJB.getCountries(request1);
-            loadGenericCombobox(countries, cmbCountry, "name", evenInteger, Long.valueOf(cardParam != null ? cardParam.getPersonCustomerId().getCountryId().getId() : 0));
-        } catch (EmptyListException ex) {
-            showError(ex);
-            ex.printStackTrace();
-        } catch (GeneralException ex) {
-            showError(ex);
-            ex.printStackTrace();
-        } catch (NullParameterException ex) {
-            showError(ex);
-            ex.printStackTrace();
-        }
-    }
-
-    private void loadCmbIssuer(Integer evenInteger) {
-        //cmbIssuer
-        EJBRequest request1 = new EJBRequest();
-        List<Issuer> issuers;
-        try {
-            issuers = utilsEJB.getIssuers(request1);
-            loadGenericCombobox(issuers, cmbIssuer, "name", evenInteger, Long.valueOf(cardParam != null ? cardParam.getProgramId().getId() : 0));
-        } catch (EmptyListException ex) {
-            showError(ex);
-        } catch (GeneralException ex) {
-            showError(ex);
-        } catch (NullParameterException ex) {
-            showError(ex);
-        }
-    }
 }
