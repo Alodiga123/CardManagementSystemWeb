97bb9d40aff7e07fae6abf569167e4c5832531a7
Unknown <yoanleon2718@gmail.com>
2020-06-10 21:49:07 -0400

    Ajuste en la pantalla y el controlador de PasswordChangeRequest

--- src/java/com/alodiga/cms/web/controllers/AdminPasswordChangeRequestController.java
+++ src/java/com/alodiga/cms/web/controllers/AdminPasswordChangeRequestController.java
@@ -18,7 +18,6 @@ import org.zkoss.zul.Button;
 import org.zkoss.zul.Textbox;
 import com.alodiga.cms.web.utils.WebConstants;
 import com.cms.commons.models.ComercialAgency;
-import com.cms.commons.models.Country;
 import com.cms.commons.models.DocumentsPersonType;
 import com.cms.commons.models.Employee;
 import com.cms.commons.models.PasswordChangeRequest;
@@ -36,6 +35,7 @@ import java.util.Map;
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Intbox;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Radio;
@@ -48,7 +48,7 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
     private Textbox txtNewPassword;
     private Textbox txtRepeatNewPassword;
     private Label lblRequestNumber;
-    private Label lblRequestDate;
+    private Datebox dtbRequestDate;
     private Label lblIdentificationNumber;
     private Label lblUser;
     private Label lblComercialAgency;
@@ -60,6 +60,7 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
     private Button btnSave;
     private Integer eventType;
     private Toolbarbutton tbbTitle;
+    private User user;
     
     @Override
     public void doAfterCompose(Component comp) throws Exception {
@@ -91,6 +92,7 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
                 break;
         }
         try {
+            user = (User) session.getAttribute(Constants.USER_OBJ_SESSION);
             utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
             personEJB = (PersonEJB) EJBServiceLocator.getInstance().get(EjbConstants.PERSON_EJB);
             loadData();
@@ -99,13 +101,12 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
         }
     }
     
-    
     public void clearFields() {
         txtCurrentPassword.setRawValue(null);
         txtNewPassword.setRawValue(null);
         txtRepeatNewPassword.setRawValue(null);
         lblRequestNumber.setValue(null);
-        lblRequestDate.setValue(null);
+        dtbRequestDate.setValue(null);
         lblIdentificationNumber.setValue(null);
         lblUser.setValue(null);
         lblComercialAgency.setValue(null);
@@ -115,33 +116,26 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
     private void loadFields(PasswordChangeRequest passwordChangeRequest) {
 
         try {
+            lblRequestNumber.setValue(passwordChangeRequest.getRequestNumber().toString());
+            dtbRequestDate.setValue(passwordChangeRequest.getRequestDate());
+            lblIdentificationNumber.setValue(passwordChangeRequest.getUserid().getIdentificationNumber());
+            lblUser.setValue(passwordChangeRequest.getUserid().getFirstNames());
+//            + " " + passwordChangeRequest.getUserid().getLastNames());
+            lblComercialAgency.setValue(passwordChangeRequest.getUserid().getComercialAgencyId().getName());
             txtCurrentPassword.setText(passwordChangeRequest.getCurrentPassword());
             txtNewPassword.setText(passwordChangeRequest.getNewPassword());
             txtRepeatNewPassword.setText(passwordChangeRequest.getNewPassword());
-            lblRequestNumber.setValue(passwordChangeRequest.getRequestNumber().toString());
-            lblRequestDate.setValue(passwordChangeRequest.getRequestDate().toString());
-            lblIdentificationNumber.setValue(passwordChangeRequest.getUserid().getIdentificationNumber());
-            lblUser.setValue(passwordChangeRequest.getUserid().getEmployeeId().getFirstNames());
-//            + " " + passwordChangeRequest.getUserid().getEmployeeId().getLastNames());
-//            + " " + passwordChangeRequest.getUserid().getLastNames());
-            lblComercialAgency.setValue(passwordChangeRequest.getUserid().getComercialAgencyId().getName());
+                  
+              if (passwordChangeRequest.getCurrentPassword() != null ) {
+                    txtCurrentPassword.setValue(passwordChangeRequest.getCurrentPassword());
+              }
+              
               if (passwordChangeRequest.getCurrentPassword() == null) {
                     txtCurrentPassword.setValue(passwordChangeRequest.getNewPassword());
-                }
+              } 
 //              if (passwordChangeRequest.getCurrentPassword() != null) {
 //                    txtCurrentPassword.setValue(passwordChangeRequest.getNewPassword());
 //                }
-//            if (user.getEmployeeId() != null) {
-//                EJBRequest request = new EJBRequest(); 
-//                HashMap params = new HashMap();
-//                params.put(Constants.PERSON_KEY, user.getEmployeeId().getPersonId().getId());
-//                request.setParams(params);
-//                phonePersonUserList = personEJB.getPhoneByPerson(request);
-//                for (PhonePerson phoneUser : phonePersonUserList) {
-//                    phonePersonUser = phoneUser;
-//                }
-//                lblUserExtAlodiga.setValue(phonePersonUser.getExtensionPhoneNumber());
-//            }
 //            if (user.getAuthorizedEmployeeId() != null) {
 //                EJBRequest request = new EJBRequest(); 
 //                HashMap params = new HashMap();
@@ -215,42 +209,41 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
              List<Sequences> sequence = utilsEJB.getSequencesByDocumentType(request1);
              numberRequest = utilsEJB.generateNumberSequence(sequence, Constants.ORIGIN_APPLICATION_CMS_ID);
              dateRequest = new Date();
-             
-        
-//            //Obtener la clasificacion del Empleado / Usuario
-//            request1 = new EJBRequest();
-//            request1.setParam(Constants.CLASSIFICATION_PERSON_USER);
-//            PersonClassification personClassification = utilsEJB.loadPersonClassification(request1);
+             lblRequestNumber.setValue(numberRequest);
+                   
+            //Valida si la contraseña actual es correcta
+            params = new HashMap();
+            params.put(Constants.CURRENT_PASSWORD, txtCurrentPassword.getValue());
+            request1.setParams(params);
+            List<User> userList = personEJB.validatePassword(request1); 
+            
+            if (userList.size() > 0) {
+                //Guardar la Solicitud de Cambio de Contraseña en la BD
+                passwordChangeRequest.setRequestNumber(numberRequest);
+                passwordChangeRequest.setRequestDate(dateRequest);
+                passwordChangeRequest.setUserid(user);
+                passwordChangeRequest.setCurrentPassword(txtCurrentPassword.getText());
+                passwordChangeRequest.setNewPassword(txtNewPassword.getText());
+                passwordChangeRequest.setIndApproved(indApproved);
 
-            //Obtener y guardar datos del Usuario 
-//            User user = new User();
-//            user.setIdentificationNumber(lblIdentificationNumber.getValue());
-//            user.setFirstNames(lblUser.getValue());
-//            user.setComercialAgencyId(lblComercialAgency.getValue().toString());        
-////        (lblComercialAgency.getValue());
-//            person.setPersonClassificationId(personClassification);
-//            person = personEJB.savePerson(person);
-            
-            //Guarda el Cambio de Contraseña
-            passwordChangeRequest.setRequestNumber(lblRequestNumber.getValue());
-            passwordChangeRequest.setIndApproved(indApproved);
-            passwordChangeRequest.setCurrentPassword(txtCurrentPassword.getText());
-            passwordChangeRequest.setNewPassword(txtNewPassword.getText());
-            passwordChangeRequest.setNewPassword(txtRepeatNewPassword.getText()); 
-            passwordChangeRequest.setIndApproved(indApproved);
-            
-            if (eventType == WebConstants.EVENT_ADD) {
-                passwordChangeRequest.setCreateDate(new Timestamp(new Date().getTime()));
-            } else {
-                passwordChangeRequest.setUpdateDate(new Timestamp(new Date().getTime()));
+                if (eventType == WebConstants.EVENT_ADD) {
+                    passwordChangeRequest.setCreateDate(new Timestamp(new Date().getTime()));
+                } else {
+                    passwordChangeRequest.setUpdateDate(new Timestamp(new Date().getTime()));
+                }
+
+                passwordChangeRequest = personEJB.savePasswordChangeRequest(passwordChangeRequest);
+                passwordChangeRequestParam = passwordChangeRequest;
+
+                this.showMessage("sp.common.save.success", false, null);
+                btnSave.setVisible(false);
             }
-      
-            passwordChangeRequest = personEJB.savePasswordChangeRequest(passwordChangeRequest);
-            passwordChangeRequestParam = passwordChangeRequest;
-            this.showMessage("sp.common.save.success", false, null);
-            btnSave.setVisible(false);
+             
+
         } catch (WrongValueException ex) {
             showError(ex);
+        } finally {
+            this.showMessage("cms.msj.errorCurrentPasswordNotMatchInBD", false, null);
         }
     }  
     
@@ -287,11 +280,15 @@ public class AdminPasswordChangeRequestController extends GenericAbstractAdminCo
                 txtCurrentPassword.setReadonly(true);
                 txtNewPassword.setReadonly(true);
                 txtRepeatNewPassword.setDisabled(true);
+                dtbRequestDate.setReadonly(true);
                 blockFields();
                 rApprovedYes.setDisabled(true);
                 rApprovedNo.setDisabled(true);
                 break;
             case WebConstants.EVENT_ADD:
+                lblComercialAgency.setValue(user.getComercialAgencyId().getName());
+                lblUser.setValue(user.getFirstNames() + " " + user.getLastNames());
+                lblIdentificationNumber.setValue(user.getIdentificationNumber());
                 break;
             default:
                 break;
